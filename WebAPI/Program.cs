bb71f03f20ca8154f0b7bf2a53f942f608026699
bsanguineti <bdsanguineti@gmail.com>
2022-05-20 14:03:38 -0300

    Create Custom Error Middleware

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using Application;
 using Persistance;
 using Shared;
+using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +31,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseErrorHandlingMiddleware();
 
 app.MapControllers();
 
