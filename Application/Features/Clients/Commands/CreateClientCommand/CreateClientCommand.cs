fa11b2b82cd9bbd5c79818da6ed1932c066e64f1
bsanguineti <bdsanguineti@gmail.com>
2022-05-22 17:08:03 -0300

    Create Client Controller

--- Application/Features/Clients/Commands/CreateClientCommand/CreateClientCommand.cs
+++ Application/Features/Clients/Commands/CreateClientCommand/CreateClientCommand.cs
@@ -1,4 +1,7 @@
-﻿using Application.Wrappers;
+﻿using Application.Interfaces;
+using Application.Wrappers;
+using AutoMapper;
+using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Clients.Commands.CreateClientCommand
@@ -16,9 +19,24 @@ namespace Application.Features.Clients.Commands.CreateClientCommand
 
     public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, Response<int>>
     {
+        private readonly IRepositoryAsync<Client> _repositoryAsync;
+
+        private readonly IMapper _mapper;
+
+        public CreateClientCommandHandler(IRepositoryAsync<Client> repositoryAsync, IMapper mapper)
+        {
+            _repositoryAsync = repositoryAsync;
+
+            _mapper = mapper;
+        }       
+
         public async Task<Response<int>> Handle(CreateClientCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var newRegistry = _mapper.Map<Client>(request);
+
+            var data = await _repositoryAsync.AddAsync(newRegistry);
+
+            return new Response<int>(data.Id);
         }
     }
 }
---
+++ WebAPI/Controllers/v1/ClientController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Clients.Commands.CreateClientCommand;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class ClientController : BaseApiController
+    {
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ActionResult> Post(CreateClientCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
