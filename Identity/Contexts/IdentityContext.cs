f1c9756510a15f69df604ac44f8828c5b759c557
Bruno Sanguineti <SESA671943@se.com>
2022-09-14 22:47:46 -0300

    Add jwt

---
+++ Identity/Contexts/IdentityContext.cs
@@ -0,0 +1,12 @@
+﻿using Identity.Contexts.Models;
+
+namespace Identity.Contexts
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Identity/Contexts/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Identity.Contexts.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
